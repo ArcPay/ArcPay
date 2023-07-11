a75933bbd3d99a6769ffa204981995a5f6532128
blakemscurr <blakemscurr@gmail.com>
2023-07-11 16:26:00 +1200

    Tidy up mint.js

--- circuits/scripts/mint.js
+++ circuits/scripts/mint.js
@@ -25,21 +25,21 @@ const stateTree = new MerkleTree({ hasher: vmtree.poseidon, levels: 3, zero: 0 }
 const mintTree = new MerkleTree({ hasher: vmtree.poseidon, levels: 3, zero: 0 });
 
 // recipient secret keys = ['0xb4b0bf302506d14eba9970593921a0bd219a10ebf66a0367851a278f9a8c3d08', '0xb81676dc516f1e4dcec657669e30d31e4454e67aa98574eca670b4509878290c']
-const data = [{recipient: 802933809494131860455082925493303288586736066170n, leaf_coins:[0, 10]}, {recipient: 253486562210967126009990789802080859110172940592n, leaf_coins: [11, 15]}]
-const leaves = data.map((v) => vmtree.poseidon([v.recipient, v.leaf_coins[0], v.leaf_coins[1]]))
+const requests = [{recipient: 802933809494131860455082925493303288586736066170n, leaf_coins:[0, 10]}, {recipient: 253486562210967126009990789802080859110172940592n, leaf_coins: [11, 15]}]
+requests.forEach((v) => v.leaf = vmtree.poseidon([v.recipient, v.leaf_coins[0], v.leaf_coins[1]]))
 
 // fill up mint tree
-leaves.forEach((leaf, i) => {
-    mintTree.update(i, leaf)
+requests.forEach((request, i) => {
+    mintTree.update(i, request.leaf)
 })
 
 // update state tree while emptying mint tree and generating circuit inputs
-let inputs = data.map((datum, i) => {
+let inputs = requests.map((request, i) => {
     stateTree.update(i, 0); // initialises the leaf
-
+    
     const {pathIndices: pi, pathElements: pe} = mintTree.path(i);
     expect(verifyMerkleProof({
-        leaf: leaves[i],
+        leaf: request.leaf,
         root: mintTree.root,
         pathElements: pe,
         pathIndices: pi
@@ -49,8 +49,8 @@ let inputs = data.map((datum, i) => {
     const input = vmtree.utils.stringifyBigInts({
         step_in: [mintTree.root, stateTree.root],
         sender: 0,
-        recipient: datum.recipient,
-        leaf_coins: datum.leaf_coins,
+        recipient: request.recipient,
+        leaf_coins: request.leaf_coins,
         mintPathElements: pe,
         mintPathIndices: pi,
         pathElements: se,
@@ -59,7 +59,7 @@ let inputs = data.map((datum, i) => {
     
     // prepare trees for next mint
     mintTree.update(i,0);
-    stateTree.update(i, leaves[i]);
+    stateTree.update(i, request.leaf);
     return input;
 })
 
